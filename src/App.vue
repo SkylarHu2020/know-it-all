1895f339e34ff0a4387ecb120135fe15bea4d175
SkylarHu2020 <u3541891@connect.hku.hk>
2020-10-19 23:13:50 +0800

    sign in form update

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,67 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list='list'></column-list>
+    <!-- <column-list :list='list'></column-list> -->
+    <validate-form @form-submit="onFormSubmit">
+      <div class="mb-3">
+        <label class="form-label">Email Address</label>
+        <validate-input
+          :rules="emailRules"
+          v-model="emailVal"
+          placeholder="please enter your email"
+          type="text"
+          ref="inputRef"
+        />
+      </div>
+      <div class="form-label">
+        <label class="form-label">Password</label>
+        <validate-input
+          type="password"
+          placeholder="Please enter your password"
+          :rules="passwordRules"
+          v-model='passwordVal'
+        />
+      </div>
+      <template #submit>
+        <span class="btn btn-danger">Submit</span>
+      </template>
+    </validate-form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from '@/components/ComlumnList.vue'
+// import ColumnList, { ColumnProps } from '@/components/ComlumnList.vue'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
+import ValidateForm from '@/components/ValidateForm.vue'
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 3,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 4,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
+//     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   }
+// ]
 
 const currentUser: UserProps = {
   isLogin: true,
@@ -45,13 +71,37 @@ const currentUser: UserProps = {
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
-    GlobalHeader
+    // ColumnList,
+    GlobalHeader,
+    ValidateInput,
+    ValidateForm
   },
   setup() {
+    const inputRef = ref<any>()
+    const emailVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: 'The email can not be empty.' },
+      { type: 'email', message: 'Please enter a valid email.' }
+    ]
+    const passwordVal = ref('')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: 'The password can not be empty' }
+    ]
+
+    const onFormSubmit = (result: boolean) => {
+      // TODO: next
+      console.log('result', result)
+    }
+
     return {
-      list: testData,
-      currentUser: currentUser
+      // list: testData,
+      currentUser: currentUser,
+      emailRules,
+      emailVal,
+      passwordVal,
+      passwordRules,
+      onFormSubmit,
+      inputRef
     }
   }
 })
